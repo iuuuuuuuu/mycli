5e6595c445c8d68a079066c609b9c5ddf5ae2466
iuuuuuuuu <1524514078@qq.com>
2023-03-29 10:12:59 +0800

    更改文件名

--- bin/cli.js
+++ bin/cli.js
@@ -4,7 +4,7 @@ console.log('mycli is running');
 const { program } = require('commander');
 
 require('../lib/core/help.js')(program);
-require('../lib/core/mycommander.js')(program);
+require('../lib/core/commander.js')(program);
 
 // name 必选 <> 里面是必选参数 [name] 意思就是可选参数
 
